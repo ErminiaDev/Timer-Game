02051c568b5e9d0b4f130fe1f8659add423efbed
ErminiaDev <jeanningros.marine@gmail.com>
2024-02-27 18:38:23 +0100

    add challenges

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,37 @@
+import { useState, useRef } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  //if you have a value that you want to manage but not trigger a UI update
+  const timer = useRef();
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
+    </section>
+  );
+}
